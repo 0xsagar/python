95a9378a49beeaba28df9bc5b2427ea8b767150d
0xsagar <sagar.19bct@cmr.edu.in>
2022-03-01 00:40:58 +0530

    python day1

---
+++ CodeWithHarry/02_ps1.py
@@ -0,0 +1,26 @@
+# # Write a program to print Twinkle Twinkle Poem
+# print('\nTwinkle Twinkle Little Stars')
+# print('How I wonder What You Are')
+# print('Up Above The World So High')
+# print('Like A Diamond In The Sky\n')
+
+# # Import os module and do something
+# import os
+# # # Method 1
+# # cwd = os.getcwd()
+# # print(cwd)
+# # Method 2
+# print(os.getcwd())
+
+# # Use a module
+# from playsound import playsound
+# playsound('//home//kali//Music//Fursat - Arjun Kanungo _ Official Lyric Video.m4a')
+
+
+# # Write content directory using os function
+# Author: Sagar
+# Location: Earth
+# Date: 01/03/2022
+# import os #importing os module
+# d = os.listdir() # command
+# print(d)
