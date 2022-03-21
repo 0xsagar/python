f04cc333c98e2ac0e03fc1264e4f33148820d56b
0xsagar <sagar.19bct@cmr.edu.in>
2022-03-22 00:55:46 +0530

    lists and methods

---
+++ CodeWithHarry/09_lists.py
@@ -0,0 +1,12 @@
+# a = [1, 2, 3, 5, 9, 21]
+
+# print(a)
+
+# print(a[2])
+
+# a[0] = 99
+# print(a)
+
+
+# b = [1, "Sagar", 20.04, True]
+# print(b)
---
+++ CodeWithHarry/10_list_methods.py
@@ -0,0 +1,21 @@
+# # sorting - changes the list - does not give any output
+# l1 = [1, 6, 3, 8, 2, 19, 11]
+# # l1.sort()
+# # print(l1)
+
+# # l1.reverse()
+# # print(l1)
+
+# # append - adds the number at the end of the list
+# # l1.append(25)
+# # print(l1)
+
+# # insert - inserts a number between index:n and index: n-1 -- insert(n, number)
+# # l1.insert(1, 544)  # inserts 544 at index 1
+# # print(l1)
+
+# # l1.pop(2)  # deletes the number at index 2
+# # print(l1)
+
+# l1.remove(19)  # deletes a particular number from the list
+# print(l1)
