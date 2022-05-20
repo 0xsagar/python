df40501e497bff51f7294dfc135c3add5ad41b99
0xsagar <sagar.19bct@cmr.edu.in>
2022-05-20 22:37:29 +0530

    ..

---
+++ CodeWithHarry/16_sets.py
@@ -0,0 +1,10 @@
+a = {1, 2, 3, 4, 1}
+
+print(type(a))
+print(a)
+# Set is a collecion of non-repetetive items
+# Sets do not have/print  repetetive items
+
+# Important : This empty shows a dict and not a set
+b = {}
+print(type(b))
