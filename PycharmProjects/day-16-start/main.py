cb7b5de472d3bb85b9e955e06227bf71fd9b3260
0xsagar <sagar.19bct@cmr.edu.in>
2022-08-02 01:40:55 +0530

    ..

--- PycharmProjects/day-16-start/main.py
+++ PycharmProjects/day-16-start/main.py
@@ -1,14 +1,22 @@
 from turtle import Turtle, Screen
 import another_module
-print(another_module.another_variable)
+# print(another_module.another_variable)
+#
+# timmy = Turtle()
+# print(timmy)
+#
+# timmy.shape("turtle")
+# timmy.color("blue4")
+# timmy.forward(100)
+#
+# my_screen = Screen()
+# print(my_screen.canvheight)
+# my_screen.exitonclick()
 
-timmy = Turtle()
-print(timmy)
-
-timmy.shape("turtle")
-timmy.color("blue4")
-timmy.forward(100)
-
-my_screen = Screen()
-print(my_screen.canvheight)
-my_screen.exitonclick()
+from prettytable import PrettyTable
+table = PrettyTable()
+table.add_column("Pokemon Name", ["Pikachu", "Squirtle", "Charmander"])
+table.add_column("Type", ["Electric", "Water", "Fire"])
+print(table)
+table.align = "l"
+print(table)
