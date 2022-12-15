b7e02f0eb691354c5e06c985432d08fcd35e84f2
0xsagar <sagar.19bct@cmr.edu.in>
2022-12-16 00:36:41 +0530

    random

--- PycharmProjects/day-16-start/main.py
+++ PycharmProjects/day-16-start/main.py
@@ -1,3 +1,4 @@
+import turtle
 from turtle import Turtle, Screen
 import another_module
 # print(another_module.another_variable)
@@ -50,3 +51,68 @@ import another_module
 # import turtle as t
 # tim = t.Turtle()
 
+#import heroes
+# print(heroes.gen())
+from turtle import Turtle, Screen
+import random
+tim = Turtle()
+# for _ in range(15):
+#     tim.forward(10)
+#     tim.penup()
+#     tim.forward(10)
+#     tim.pendown()
+
+# colors=["medium aquamarine", "wheat", "SeaGreen", "LightSeaGreen", "IndianRed", "DeepSkyBlue", "DarkOrchid", "SlateGrey"]
+# for _ in range(3,11):
+#     tim.color(random.choice(colors))
+#     a = 360/_
+#     for n in range(_):
+#         tim.forward(100)
+#         tim.right(a)
+
+
+# colors=["medium aquamarine", "wheat", "SeaGreen", "LightSeaGreen", "IndianRed", "DeepSkyBlue", "DarkOrchid", "SlateGrey"]
+# direction = [0, 90, 180, 270]
+# tim.pensize(10)
+# tim.speed(speed="fast")
+# for _ in range(69):
+#     tim.color(random.choice(colors))
+#     tim.forward(40)
+#     tim.setheading(random.choice(direction))
+
+
+
+# direction = [0, 90, 180, 270]
+# tim.pensize(10)
+# tim.speed(speed="fastest")
+# turtle.colormode(255)
+# def diff_color():
+#     r = random.randint(0, 255)
+#     g = random.randint(0, 255)
+#     b = random.randint(0, 255)
+#     random_color = (r, g, b)
+#     return random_color
+#
+# for _ in range(100):
+#     tim.color(diff_color())
+#     tim.forward(40)
+#     tim.setheading(random.choice(direction))
+
+
+# tim.speed(speed="fastest")
+# turtle.colormode(255)
+# def different_color():
+#     r = random.randint(0, 255)
+#     g = random.randint(0, 255)
+#     b = random.randint(0, 255)
+#     random_color = (r, g, b)
+#     return random_color
+#
+# def draw_spirograph(size_of_gap):
+#     for _ in range(int(360/size_of_gap)):
+#         tim.color(different_color())
+#         tim.circle(100)
+#         tim.right(size_of_gap)
+# draw_spirograph(5)
+screen=turtle.Screen()
+screen.exitonclick()
