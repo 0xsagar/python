1e384fd170b022ebaf27749d8f4c77b26ca754ec
0xsagar <sagar.19bct@cmr.edu.in>
2022-12-15 23:29:17 +0530

    ..

--- PycharmProjects/day-16-start/main.py
+++ PycharmProjects/day-16-start/main.py
@@ -13,10 +13,40 @@ import another_module
 # print(my_screen.canvheight)
 # my_screen.exitonclick()
 
-from prettytable import PrettyTable
-table = PrettyTable()
-table.add_column("Pokemon Name", ["Pikachu", "Squirtle", "Charmander"])
-table.add_column("Type", ["Electric", "Water", "Fire"])
-print(table)
-table.align = "l"
-print(table)
+# from prettytable import PrettyTable
+# table = PrettyTable()
+# table.add_column("Pokemon Name", ["Pikachu", "Squirtle", "Charmander"])
+# table.add_column("Type", ["Electric", "Water", "Fire"])
+# print(table)
+# table.align = "l"
+# print(table)
+
+# from turtle import Turtle, Screen
+# timmy_the_turtle = Turtle()
+# timmy_the_turtle.forward(100)
+# timmy_the_turtle.right(90)
+# timmy_the_turtle.forward(100)
+# timmy_the_turtle.right(90)
+# timmy_the_turtle.forward(100)
+# timmy_the_turtle.right(90)
+# timmy_the_turtle.forward(100)
+# timmy_the_turtle.right(90)
+
+# for a in range(4):
+#     timmy_the_turtle.forward(100)
+#     timmy_the_turtle.right(90)
+
+# Three methods to import module
+# import turtle
+# tim = turtle.Turtle()
+
+# from turtle import Turtle
+# tom = Turtle()
+
+# from turtle import *
+# tum = Turtle()
+
+# Rename the module name (used when the name of the module is too big)
+# import turtle as t
+# tim = t.Turtle()
+
