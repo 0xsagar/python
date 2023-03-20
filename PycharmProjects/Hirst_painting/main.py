f48d4aa382d6c4a78454f1680741912b9892dbc6
0xsagar <sagar.19bct@cmr.edu.in>
2023-03-21 00:47:59 +0530

    ..

--- PycharmProjects/Hirst_painting/main.py
+++ PycharmProjects/Hirst_painting/main.py
@@ -2,12 +2,27 @@ import turtle as turtle_module
 import random
 tim = turtle_module.Turtle()
 turtle_module.colormode(255)
+tim.speed("fastest")
+tim.penup()
+tim.hideturtle()
 color_list = [(232, 251, 242), (198, 12, 32), (250, 237, 17), (39, 76, 189), (38, 217, 68), (238, 227, 5),
               (229, 159, 46), (27, 40, 157), (215, 74, 12), (15, 154, 16), (199, 14, 10), (242, 246, 252),
               (243, 33, 165)]
 
-for _ in range(10):
+tim.setheading(225)
+tim.forward(300)
+tim.setheading(0)
+number_of_dots = 100
+
+for dot_count in range(1, number_of_dots + 1):
     tim.dot(20, random.choice(color_list))
     tim.forward(50)
+    if dot_count % 10 == 0:
+        tim.setheading(90)
+        tim.forward(50)
+        tim.setheading(180)
+        tim.forward(500)
+        tim.setheading(0)
+
 screen = turtle_module.Screen()
 screen.exitonclick()
