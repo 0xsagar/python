5a9b81f92eda19e3451818d634d1abfad339abc0
0xsagar <sagar.19bct@cmr.edu.in>
2023-05-06 13:18:06 +0530

    day 21 completed

---
+++ PycharmProjects/SnakeGame/scoreboard.py
@@ -0,0 +1,26 @@
+from turtle import Turtle
+ALIGNMENT = "center"
+FONT = ("Courier", 16, "normal")
+
+
+class Scoreboard(Turtle):
+
+    def __init__(self):
+        super().__init__()
+        self.score = 0
+        self.penup()
+        self.hideturtle()
+        self.color("white")
+        self.goto(0, 265)
+        self.write(f"Score : {self.score}", align=ALIGNMENT, font=FONT)
+
+    def increase_score(self):
+        self.score += 1
+        self.clear()
+        self.write(f"Score : {self.score}", align=ALIGNMENT, font=FONT)
+
+    def game_over(self):
+        self.clear()
+        self.goto(0, 0)
+        self.write(f"Game Over! Your final score is : {self.score}", align=ALIGNMENT, font=FONT)
+
