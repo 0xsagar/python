cafae4977729ec6ee7c78ae976378cb46d586b0c
0xsagar <sagar.19bct@cmr.edu.in>
2023-06-22 23:23:07 +0530

    ..

--- CodeWithHarry/18_conditions.py
+++ CodeWithHarry/18_conditions.py
@@ -1,5 +1,22 @@
-age = int(input("Enter your age: "))
-if age >= 18:
-    print("Yes")
+# age = int(input("Enter your age: "))
+# if age >= 18:
+#     print("Yes")
+# else:
+#     print("No")
+
+# b = None
+# if(b is None):
+#     print("Yes")
+# else:
+#     print("No")
+
+c = [1,2,3,4,5,6]
+print(6 in c)
+
+d = 7
+if(d==7):
+    print("if and elif are mandatory")
+elif(d>=7):
+    print("elif is mandatory, we can write as much elif as we want")
 else:
-    print("No")
+    print("Else if OPTIONAL, executed only if all the if-elif fail")
---
+++ CodeWithHarry/19_ps5.py
@@ -0,0 +1,15 @@
+#Q1: WAP to find the greatest of 4 numbers entered by a user?
+# a = []
+# for x in range(4):
+#     a.append(int(input("Enter number: ")))
+# greatest = -999999
+# for x in range(4):
+#     if(a[x] > greatest):
+#         greatest = a[x]
+# print("The greatest number is: ", greatest)
+
+#Q2: Check if a student is pass or fail, min 33% each subject and total 40% is required to pass the class.
+b = []
+subject = ["English", "Maths", "Science"]
+for x in range(subject[x]):
+    b.append(int(input(f"Enter your marks in {x}: ")))
