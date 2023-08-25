f58df6066c23e62c1041a99780879286636864f1
0xsagar <sagar.19bct@cmr.edu.in>
2023-08-25 19:29:49 +0530

    ..

--- CodeWithHarry/26_files.py
+++ CodeWithHarry/26_files.py
@@ -1,9 +1,28 @@
+# Read in a file
 # f = open('/home/kali/Programming/python/CodeWithHarry/sample.txt', 'r')
-f = open('/home/kali/Programming/python/CodeWithHarry/sample.txt') # Default vault will be set to = read 'r'
+# f = open('/home/kali/Programming/python/CodeWithHarry/sample.txt') # Default vault will be set to = read 'r'
 # data = f.read()
 # data = f.read(5) # Reads first 5 characters
-data = f.readline() # Reads the first line
-print(data)
-data = f.readline() # if written 2nd time, will read the 2nd line
-print(data)
+# data = f.readline() # Reads the first line
+# print(data)
+# data = f.readline() # if written 2nd time, will read the 2nd line
+# print(data)
+# f.close()
+
+# Write in a file
+# f = open("another.txt", "w")
+# f.write("This is how you can write in that file")
+# If the file is not present, the code will create a new file, if ran multiple times, it will overwrite that file
+
+# Append into that file
+f = open("another.txt", "a")
+f.write("I an appending into that file")
 f.close()
+# Now this text will be appended into that another.txt file, if ran multiple times, it will paste the same text at the end multiple times
+
+# The best way to open and close a file is using the 'with' statement (Here we don't need to write f.close, it'll be closed autimatically)
+with open("another.txt", "r") as f:
+    a = f.read()
+with open("another.txt", "w") as f:
+    a = f.write("Hello")
+print(a)
---
+++ CodeWithHarry/27_ps9.py
@@ -0,0 +1,23 @@
+# Q1: WAP to read the text from a file poem.txt and find whether it has word 'twinkle' in it.
+# with open("poem.txt", "r") as f:
+#     a = f.read()
+# if 'twinkle' in a:
+#     print("twinkle is present")
+# else:
+#     print("twinkle is not present")
+
+# Q2: Update highscore if more that previous data
+# def game():
+#     return 6421
+
+# score = game()
+# with open("highscore.txt")as f:
+#     highScoreString = f.read()
+# if highScoreString=='':
+#     with open("highscore.txt", "w") as f:
+#         f.write(str(score))
+# elif int(highScoreString) < score:
+#     with open("highscore.txt", "w") as f:
+#         f.write(str(score))
+
+# Q3: WAP to write multiplication table from 2 to 20 in a folder in different files
